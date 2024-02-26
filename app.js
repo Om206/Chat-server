b4db04024ce5fc0de2de0ee61ce2db19b1dd0ed5
Om206 <94994422+Om206@users.noreply.github.com>
2024-02-26 09:58:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const { Server } = require("socket.io");
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chat-application-five-eta.vercel.app",
   },
 });
 
